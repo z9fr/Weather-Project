c8511981dff7de1727676eeecd951ebc3c87f931
Dasith Vidanage <dasithvidanage000@gmail.com>
2021-07-04 06:36:50 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace weatherapp
     static class Program
     {
         /// <summary>
-        /// The main entry point for the application.
+        /// The main entry point for the application
         /// </summary>
         [STAThread]
         static void Main()
